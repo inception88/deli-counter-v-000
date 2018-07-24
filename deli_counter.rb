127fb6b174dcb2d0227e43b7215c6d6750bb78bf
Benjamin Jones <bljones36@gmail.com>
2018-07-24 23:29:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,12 @@
-def line()
+def line(lineup)
+  if lineup.length < 1
   puts "The line is currently empty."
+  else
+    x = 0
+  output = "The line is currently:"
+  while x < lineup.length
+    output = output + " #{x+1}. #{lineup[x]}"
+    x += 1
+  end
+  puts output
 end
